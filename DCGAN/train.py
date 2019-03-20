dfa95db04626f54ddbb948145c4b0f3fa89c41e7
sanchit2843 <sanchittanwar75@gmail.com>
2019-03-20 14:09:08 +0530

    Train and utils complete

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -1,9 +1,23 @@
 import datacreation
 from dataloader import dataloader
 from model import Generator,Discriminator
+import utils
+import matplotlib.pyplot as plt
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#generator object
+gen = Generator().to(device)
+#applying desired weights
+gen.apply(utils.weights_init)
+#discriminator object
+dis = Discriminator().to(device)
+#applying desired weights
+dis.apply(utils.weights_init)
 Tensor = torch.cuda.FloatTensor if 'cuda' else torch.FloatTensor
-for epoch in range(100):
+g_losses = []
+d_losses = []
+
+for epoch in range(1000):
     for i, imgs in enumerate(dataloader):
 
         valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
@@ -11,27 +25,26 @@ for epoch in range(100):
 
         real_imgs = Variable(imgs.type(Tensor))
         optimizer_G.zero_grad()
-        # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0],256,8,8))))
-        # Generate a batch of images
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0],100,1,1))))
         gen_imgs = gen(z)
-        b = dis(gen_imgs)
+        b = dis1(gen_imgs)
         g_loss = adversarial_loss(b, valid)
-
         g_loss.backward()
         optimizer_G.step()
 
         optimizer_D.zero_grad()
-        real_loss = adversarial_loss(dis(real_imgs), valid)
-        fake_loss = adversarial_loss(dis(gen_imgs.detach()), fake)
+        real_loss = adversarial_loss(dis1(real_imgs), valid)
+        fake_loss = adversarial_loss(dis1(gen_imgs.detach()), fake)
         d_loss = (real_loss + fake_loss) / 2
 
         d_loss.backward()
         optimizer_D.step()
-    a = gen_imgs.cpu().detach().numpy()
-    a = a[10]
-    a = np.reshape(a,(158,158,3))
-    imshow(a)
-    show()
-    print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, 100, i, len(dataloader),
+
+    if(epoch%10==0):
+      a = utils.im_convert(gen_imgs)
+      plt.imshow(a)
+      plt.show()
+    g_losses.append(g_loss.item())
+    d_losses.append(d_loss.item())
+    print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, 1000, i, len(dataloader),
                                                             d_loss.item(), g_loss.item()))
---
+++ DCGAN/utils.py
@@ -0,0 +1,30 @@
+#initialize weights
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+#converting image in matplotlib format in cpu
+def im_convert(tensor):
+    image = tensor.to("cpu").clone().detach()
+    image = image.numpy().squeeze()
+    image = image[0]
+    image = image.transpose(1,2,0)
+    image = image * np.array((0.5,0.5,0.5)) + np.array((0.5,0.5,0.5))
+    image = image.clip(0, 1)
+    return image
+
+#plot error
+def error_plot(dis_loss, gen_loss):
+    plt.figure(figsize=(10,5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(gen_loss,label="G")
+    plt.plot(dis_loss,label="D")
+    plt.xlabel("epochs")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.show()
+#plot random generated images
